b1a049378973903e0d65e6421f9bfbe2897feb65
Jefferson Smith <Jeffersonneilsmith@gmail.com>
2025-07-06 00:34:26 -0400

    updated the next.configs.mjs file to support @/ as root

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
+// next.config.mjs
+import path from 'path'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:['lh3.googleusercontent.com']
-    }
-};
+  images: {
+    domains: ['lh3.googleusercontent.com']
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(process.cwd())
+    return config
+  }
+}
 
-export default nextConfig; 
+export default nextConfig
